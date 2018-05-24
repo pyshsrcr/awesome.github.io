365555671094bd7dddafd9c92808f30c65d469aa
pyshsrcr <39090406+pyshsrcr@users.noreply.github.com>
2018-05-24 21:12:51 +0530

    more files and edits (#3)

--- bundle.js
+++ bundle.js
@@ -17108,8 +17108,11 @@
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],2:[function(require,module,exports){
-var _ = require('lodash');
-console.log(_);
+
+var _ = require("lodash");
+var array = [1,2,3,4,5,6,7,8,9];
+console.log('answer:',_.without(array,3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
