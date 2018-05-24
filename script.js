c031007994ff1a4754fd90111089eec591e91a15
pyshsrcr <pyshsrcr@gmail.com>
2018-05-24 07:48:07 +0530

    new files added

---
+++ script.js
@@ -0,0 +1,28 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var body = document.getElementById("gradient");
+var text1 = document.getElementById("textcolor");
+var tcolor = document.getElementById("tcolor"); 
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function setTextColor(){
+	text1.style.color = color3.value;
+	tcolor.textContent= "value="+color3.value;
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+color3.addEventListener("input", setTextColor);
